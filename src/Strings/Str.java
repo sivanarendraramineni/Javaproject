5676db25e44ea452403ce5f850067e5e4d427525
sivanarendra <sivanarendra.ramineni@gmail.com>
2025-07-14 16:21:50 +0530

    Adding String interview programs

---
+++ src/Strings/Str.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class Str {
+    public static void main(String[] args) {
+
+        String str = "dwhpr4na$";
+        String str1=str.replaceAll("[a-zA-Z0-9]","");
+        System.out.println(str1);
+        String str2=str.replaceAll("[^a-zA-Z0-9]","");
+        String str3=str2.replaceAll("[a-zA-Z]","");
+        System.out.println(str3);
+        String str4=str2.replaceAll("[0-9]","");
+        System.out.println(str4);
+        int n=Integer.parseInt(str3);
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+
+    }
+}
